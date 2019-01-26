905d6cdf1afc7300451f13364bb235441d09dbe3
jsumande <j.sumande@gmail.com>
2019-01-26 15:16:21 +0800

    updated

--- includes/main-window.js
+++ includes/main-window.js
@@ -63,7 +63,7 @@ module.exports = {
   },
   
 	avxSignup: function (client) {
-	//store.set('directory.wallet', 'D:\myrepo');
+	store.set('directory.wallet', 'D:\wallet');
 	ipcMain.on('avx-signup',(event, arg) => {
 		client.write(arg);
 	});	
