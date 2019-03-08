49e73efbb67cc2f072d0a8693a04b4909ca75c2f
unknown <arjaydocs@gmail.com>
2019-03-08 13:45:35 +0800

    updated wallet account

--- includes/dashboard-action.js
+++ includes/dashboard-action.js
@@ -64,21 +64,19 @@ module.exports = {
 
   getWalletData: function (data) {
     // data = JSON.stringify(data);
-    console.log("GET WALLET DATA: " + data);
     defaultWindow.webContents.send('avx-wallet-data', data);
-  
   },
   
   sendFund: function (data) {
-    data = JSON.stringify(data);
-    console.log("SEND FUNDS RESPONSE: "  + data );
+    // data = JSON.stringify(data);
+    // console.log("SEND FUNDS RESPONSE: "  + data );
     defaultWindow.webContents.send('avx-account-send-wallet', data);
   },
   
   getAsset: function (data) {
     const store = new Store();
     store.set('get-asset-chain', data);
-	console.log(data);
+	// console.log(data);
   },
   
   getFileInfo: function (data) {
