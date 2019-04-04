7cd49d3a462a3ffecc3cfa31f178852a6bd629d0
jsumande <j.sumande@gmail.com>
2019-04-04 15:13:52 +0800

    added functions for downloading lists

--- js/account-file.js
+++ js/account-file.js
@@ -21,58 +21,57 @@ $(document).ready( () => {
 });
 
 ipcRenderer.on('receive-filetransfer-stats', (event, arg) => {
-   // console.log(JSON.stringify(arg));
+    console.log(JSON.stringify(arg));
 	//console.log( Object.keys(arg).length );
 	let fn  = arg["filename"];
-	if (!fileDownloadList[fn]) {
-		fileDownloadList[fn] = {
-			data_rate: arg["data_rate"],
-			progress: arg["progress"],
-			peers: arg["peers"]
-		};
-		
-		let row = '';
-	
-		for (var key in fileDownloadList) {
 
-			row += '<tr>';
-			row += '<th scope="row"></th>';
-			row += '<td id="stat-filename" >' + key + '</td>';
-			row += '<td><div id="stat-datarate">' + fileDownloadList[key].data_rate + '</div></td>';
-			row += '<td>';
-			row += '<div class="container">';
-			row += '<span class="progress-label">'+fileDownloadList[key].progress.toFixed(2) + "%"+'</span>';
-			row += '<div class="progress">';
-			row += '<div id="stat-progress" class="progress-bar bg-success" style="width:'+ fileDownloadList[key].progress +'%" id="prog-bar" role="progressbar" aria-valuemin="0" aria-valuenow="'+ fileDownloadList[key].progress +'" aria-valuemax="100">';
-			row += '</div>';
-			row += '</div>';
-			row += '</div>';
-			row += '</td>';
-			row += '<td><div id="stat-peers"> ' + fileDownloadList[key].peers + '</div></td>';
-			row += '<td class="action-col" id="action-col-id-1">';
-			row += '<a href="#"><span class="icon-segoe segoe-v-player">&nbsp;</span></a>';
-			row += '<a href="#"> <span class="mdi mdi-pause">&nbsp;</span></a>';
-			row += '<a href="#"> <span class="mdi mdi-trash-can-outline">&nbsp;</span></a>';
-			row += '<a href="#"> <span class="mdi mdi-file-remove">&nbsp;</span></a>';
-			row += '</td>';
-			row += '</tr>';
-		}
-		
-		$("#dtVerticalScroll tbody").append(row);
-	} else {
+	//if (!fileDownloadList[fn]) {
 		fileDownloadList[fn] = {
 			data_rate: arg["data_rate"],
 			progress: arg["progress"],
 			peers: arg["peers"]
 		};
-		
-		$("#stat-filename").html(fn);
-		$("#stat-datarate").html(fileDownloadList[fn].data_rate);
-		$("#stat-progress").attr("style", "width: "+fileDownloadList[fn].progress + "%");
-		$("#stat-progress").attr("aria-valuenow", fileDownloadList[fn].progress );
-		$(".progress-label").html(fileDownloadList[fn].progress.toFixed(2) + "%");
-		$("#stat-peers").html(fileDownloadList[fn].peers);
+	//}
+
+	let row = '';
+
+	for (var key in fileDownloadList) {
+		row += '<tr>';
+		row += '<th scope="row"></th>';
+		row += '<td id="stat-filename" >' + key + '</td>';
+		row += '<td><div id="stat-datarate">' + fileDownloadList[key].data_rate + '</div></td>';
+		row += '<td>';
+		row += '<div class="container">';
+		row += '<span class="progress-label">'+fileDownloadList[key].progress.toFixed(2) + "%"+'</span>';
+		row += '<div class="progress">';
+		row += '<div id="stat-progress" class="progress-bar bg-success" style="width:'+ fileDownloadList[key].progress +'%" id="prog-bar" role="progressbar" aria-valuemin="0" aria-valuenow="'+ fileDownloadList[key].progress +'" aria-valuemax="100">';
+		row += '</div>';
+		row += '</div>';
+		row += '</div>';
+		row += '</td>';
+		row += '<td><div id="stat-peers"> ' + fileDownloadList[key].peers + '</div></td>';
+		row += '<td class="action-col" id="action-col-id-1">';
+		row += '<a href="#"><span class="icon-segoe segoe-v-player">&nbsp;</span></a>';
+		row += '<a href="#"> <span class="mdi mdi-pause">&nbsp;</span></a>';
+		row += '<a href="#"> <span class="mdi mdi-trash-can-outline">&nbsp;</span></a>';
+		row += '<a href="#"> <span class="mdi mdi-file-remove">&nbsp;</span></a>';
+		row += '</td>';
+		row += '</tr>';
 	}
+	
+	$("#dtVerticalScroll tbody").html(row);
+	console.log('true');
+	
+	/*else {
+		for (var key in fileDownloadList) {
+			$("#stat-filename").html(key);
+			$("#stat-datarate").html(fileDownloadList[key].data_rate);
+			$("#stat-progress").attr("style", "width: " + fileDownloadList[key].progress + "%");
+			$("#stat-progress").attr("aria-valuenow", fileDownloadList[key].progress );
+			$(".progress-label").html(fileDownloadList[key].progress.toFixed(2) + "%");
+			$("#stat-peers").html(fileDownloadList[key].peers);
+		}
+	}*/
 	
 	
 	
